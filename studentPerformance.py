9f14c37f0d4a0dc10f8366c8c54b885a6f4a2381
manthan <manthanp542@gmail.com>
2025-03-22 00:40:01 -0700

    Pkl File

--- studentPerformance.py
+++ studentPerformance.py
@@ -3,6 +3,34 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+import pymysql
+
+def build_connection():
+    # Database connection parameters
+    host = "localhost"
+    port = 3306
+    user = "root"
+    password = "Manthan@456"
+    database = "student"
+    try:
+        connection = pymysql.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            database=database
+        )
+        print("Connected to MySQL database")        
+        # Perform database operations here
+    except pymysql.MySQLError as err:
+        print(f"Error: {err}")
+    finally:
+        return connection
+    
+def insert(cnx, query):
+    cursor = cnx.cursor()
+    cursor.execute(query)
+    cnx.commit()
 
 def load_model():
     with open(r'linearRegression.pkl', 'rb') as file:
@@ -11,18 +39,19 @@ def load_model():
 
 def preprocessing_input(data, scaler, le):
     print("data is", data)
-    data['Extracurricular Activities'] = le.fit_transform([data['Extracurricular Activities']])[0]
+    data['Extracurricular Activities'] = int(le.fit_transform([data['Extracurricular Activities']])[0])
     print("data1q is", data)
     df = pd.DataFrame([data])
     print("dataqwqw is", data)
     df_transformed = scaler.transform(df)
-    return df_transformed
+    return df_transformed, df
 
 def predict_data(data):
     model, scaler, le = load_model()
-    df_transformed = preprocessing_input(data, scaler, le)
+    df_transformed, df = preprocessing_input(data, scaler, le)
     prediction = model.predict(df_transformed)
-    return prediction
+    df['Performance_Index'] = int(prediction)
+    return prediction, df
 
 def main():
     st.title("Student Performance Prediction.")
@@ -35,13 +64,20 @@ def main():
     sleep = st.number_input("Sleep hours")
     sample = st.number_input("sample studied")
     if st.button("predict"):
-        user_data = {'Hours Studied': hours,
-                    'Previous Scores': prev,
+        user_data = {'Hours Studied': int(hours),
+                    'Previous Scores': int(prev),
                     'Extracurricular Activities': ext,
-                    'Sleep Hours': sleep,
-                    'Sample Question Papers Practiced': sample
+                    'Sleep Hours': int(sleep),
+                    'Sample Question Papers Practiced': int(sample)
                     }
-        prediction = predict_data(user_data)
+        prediction, df = predict_data(user_data)
+        final_values = []
+        for i in df.iloc[0].values:
+            final_values.append(int(i))
+        query = f"INSERT INTO performance (Hours_Studied,Previous_Scores,Extracurricular_Activities,Sleep_Hours,Sample_Question_Papers_Practiced,Performance_Index) VALUES {tuple(final_values)}"
+        cnx = build_connection()
+        print(f"QUERY IS: {query}")
+        insert(cnx, query)
         print(prediction)
         st.success(f"Your prediction result is: {prediction}")
 
